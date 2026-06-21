0208a3fbcbf33583257f72b9731b64e3092c26f6
lemming417 <lemming417@gmail.com>
2026-06-21 22:02:59 +0900

    Rename Transaction to MonthlyHistory and refactor methods
    Refactor Transaction class into MonthlyHistory class to manage monthly transaction history.

--- my_package/core.py
+++ my_package/core.py
@@ -1,35 +1,25 @@
-class Transaction:
-    """ 
-    가계부 작성을 위해 현재 돈과 월을 표시하는 객체를 생성하기 위한 클래스입니다.
+class MonthlyHistory:
+    """한 달 거래 내역을 관리하는 클래스
 
     :param month: 작성할 월
-    :param day: 돈이 들어오거나 나간 날
-    :param amount: 거래 내역
-    :param category: 거래 분류
-    :param memo: 거래 내용 설명
-    
+
+    :ivar month: int
+    :ivar __use_history: list
     """
-
-
-    def __init__(self, month: int, day: int, amount: int, category: str, memo: str):
-        self.amount = amount
+    def __init__(self, month: int):
         self.month = month
         self.__use_history = []
-        self.add_history(day, amount, category, memo)
-
-
 
     def add_history(self, day, amount, category, memo):
-        """
-        거래 내역을 딕셔너리 형태로 저장합니다.
+        """거래 내역을 딕셔너리 형태로 저장합니다.
 
         :param day: 사용한 날짜 기록
         :param amount: 거래 내역
         :param category: 거래 분류
         :param memo: 간단한 설명
-        
         """
         history_item = {
+            "month": self.month,
             "day": day,
             "amount": amount,
             "category": category,
@@ -38,18 +28,15 @@ class Transaction:
 
         self.__use_history.append(history_item)
 
-
     def calculate_total(self):
-        """
-        한 달간 거래 내역을 합산하여 리턴하는 함수입니다.
-        
+        """한 달간 거래 내역을 합산하여 리턴하는 함수입니다.
+
         :return: 거래 내역의 총합
         """
 
         return sum(item['amount'] for item in self.__use_history)
-    
-
 
     def get_history(self):
         """use_history에 직접적으로 접근하는 것을 막기 위해 사용합니다."""
+
         return self.__use_history.copy()
