6edb52f2dec98b8df0c96aa3d9b73ac71ac41855
lemming417 <lemming417@gmail.com>
2026-06-22 01:29:25 +0900

    Modify code to fit pycodestyle
    Updated docstring for _check_in_out function to clarify its purpose and usage.

--- my_package/utils.py
+++ my_package/utils.py
@@ -1,5 +1,13 @@
 def _check_in_out(history_list):
-    """csv 파일 저장을 위해 지출과 수입을 분리하기 위해 사용합니다."""
+    """csv 파일 저장을 위해 지출과 수입을 분리하기 위해 사용합니다.
+
+    :param history_list: 'amount' 키를 가진 거래 내역 딕셔너리들의 리스트
+    :return: 금액이 0보다 크면 Ture, 0 이하면 False로 구성된 리스트
+
+    >>> mock_history = [{'amount': 3000}, {'amount': -1500}]
+    >>> _check_in_out(mock_history)
+    [True, False]
+    """
     result = []
     for item in history_list:
         if item['amount'] > 0:
